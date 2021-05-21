d20ba0510b09c1154e2d63bb33df6576bdc4189c
Yoshi Askharoun <jjask7@gmail.com>
2021-05-21 17:58:20 -0500

    Added artist primary image endpoint; Added better error response

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask, request, Response, abort
 import urllib.request
+from urllib.error import HTTPError
 
 import musicbrainzngs
 
@@ -22,13 +23,20 @@ def get_image(mbid: str, locale: str):
     width = min(caa_supported_sizes, key=lambda x: abs(x - requested_width))
 
     # Request the image from the API and forward it to the Zune software
-    image = urllib.request.urlopen(f"http://coverartarchive.org/release/{mbid}/front-{width}")
-    return Response(image.read(), mimetype="image/jpeg")
+    try:
+        image = urllib.request.urlopen(f"http://coverartarchive.org/release/{mbid}/front-{width}")
+        return Response(image.read(), mimetype="image/jpeg")
+    except urllib.error.HTTPError as error:
+        abort(error.code)
 
 
 @app.route("/v3.2/<string:locale>/music/artist/<string:mbid>/primaryImage")
 def get_artist_primary_image(mbid: str, locale: str):
-    artist = musicbrainzngs.get_artist_by_id(mbid, ["url-rels"])["artist"]
+    try:
+        artist = musicbrainzngs.get_artist_by_id(mbid, ["url-rels"])["artist"]
+    except musicbrainzngs.ResponseError as error:
+        abort(error.cause.code)
+        return
 
     # Get the Deezer artist ID
     deezerUrls = [
